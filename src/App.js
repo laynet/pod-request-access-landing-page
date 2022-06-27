b64a37f029658ae20dc3a3b510b06e474df9e0df
Layne <layneingramtaylor@gmail.com>
2022-06-27 14:24:53 -0500

    react functionality working, absolutely no styling initiated whatsoever - today is a react day only, will style later

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import validator from 'validator';
+import './App.scss';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [input, setInput] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validator.isEmail(input)) {
+      setEmail(input);
+      setInput('');
+      setEmailError('');
+    } else {
+      setEmailError('Oops! Please check your email');
+      setEmail('');
+      setInput('');
+    }
+    console.log(input);
+    console.log(email);
+    console.log('buttfarts');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>butts</h1>
+      <form className='email-sbumit' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='email'
+          placeholder='Email address'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className='btn' type='submit'>
+          Request Access
+        </button>
+        {emailError}
+      </form>
+    </>
   );
 }
 
